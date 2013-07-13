97fb8a43263561aef04170d747e44f90629d55e7
Scott Hause <scott@jghause.com>
2013-07-12 23:48:51 -0500

    Created rails app with statys updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'f92542edbdcef5a4430629eab41d763e3e3a1ac00e0e391e13dae3c500488c80df95e8ed49b38999a159929390458ee10ca626bf083c7602162346d4d3e0be83'
